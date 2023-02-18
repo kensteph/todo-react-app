956944ea51fe4fb037c1eab36e591627069d6146
Kender ROMAIN <kenderromain@gmail.com>
2023-02-18 15:38:29 -0400

    Create TodoListHeader component.

---
+++ src/components/TodoListHeader/TodoListHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './todoListHeader.css';
+
+const TodoListHeader = () => (
+  <div className="listHeader">
+    <h1>Todos</h1>
+    <p>Items will persist in the browser local storage</p>
+  </div>
+);
+
+export default TodoListHeader;
