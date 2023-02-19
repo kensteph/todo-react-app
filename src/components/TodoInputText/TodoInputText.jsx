ff8d1d4f0ea14f1b06a24f55353c33a66678180f
Kender ROMAIN <kenderromain@gmail.com>
2023-02-18 23:22:23 -0400

    Implement add todo functionality when user submit the form.

--- src/components/TodoInputText/TodoInputText.jsx
+++ src/components/TodoInputText/TodoInputText.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-const TodoInputText = () => (
-  <div className="inputContainer">
-    <input className="inputText" type="text" placeholder="Add todo..." />
-    <span className="material-symbols-rounded">add_circle</span>
-  </div>
-);
+const TodoInputText = ({ handleClickAddTodo }) => {
+  const [inputext, setInputext] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputext.trim().length === 0) {
+      setMessage('Please enter a task...');
+    } else {
+      setMessage('');
+      handleClickAddTodo(inputext);
+      setInputext('');
+    }
+  };
+  return (
+    <>
+      <form className="inputContainer" onSubmit={handleSubmit}>
+        <input
+          className="inputText"
+          type="text"
+          placeholder="Add todo..."
+          value={inputext}
+          onChange={(e) => setInputext(e.target.value)}
+        />
+        <button type="submit">
+          <span className="material-symbols-rounded">add_circle</span>
+        </button>
+      </form>
+      <span>{message}</span>
+    </>
+  );
+};
+TodoInputText.propTypes = { handleClickAddTodo: PropTypes.func };
+TodoInputText.defaultProps = { handleClickAddTodo: () => {} };
 
 export default TodoInputText;
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,31 +1,21 @@
 import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import TodoListItems from '../TodoListItems/TodoListItems';
 import TodoListHeader from '../TodoListHeader/TodoListHeader';
 import TodoInputText from '../TodoInputText/TodoInputText';
 
 const TodoList = () => {
-  const [todos] = useState([
-    {
-      id: 1,
-      title: 'Task 1',
-      completed: true,
-    },
-    {
-      id: 2,
-      title: 'Task 2',
-      completed: true,
-    },
-    {
-      id: 3,
-      title: 'Task 3',
-      completed: true,
-    },
-  ]);
+  const [todos, setTodos] = useState([]);
+  const handleClickAddTodo = (title) => {
+    const newTodo = { id: uuidv4(), title, completed: false };
+    setTodos((prevState) => [...prevState, newTodo]);
+    console.log('TEST', newTodo);
+  };
 
   return (
     <div className="todoListContainer">
       <TodoListHeader />
-      <TodoInputText />
+      <TodoInputText handleClickAddTodo={handleClickAddTodo} />
       <TodoListItems todos={todos} />
     </div>
   );
